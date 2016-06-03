15195c7e42df332319d8579f71ca90abeca2cda7
Alexandre Kozlov <alex.kozlov@oce.com>
2016-06-03 01:36:40 -0700

    Server app sketch.

--- NanoRpc_TestServer/NanoRpc_TestServer.cpp
+++ NanoRpc_TestServer/NanoRpc_TestServer.cpp
@@ -1,18 +1,31 @@
+#include <memory>
 #include "nano_rpc.hpp"
 #include "named_pipe_connector.hpp"
 #include "named_pipe_rpc_channel.hpp"
+
+class ServerBuilder {
+public:
+  ServerBuilder() {}
+  void SetChannel(NanoRpc::RpcChannel &channel);
+  void RegisterService(NanoRpc::IRpcStub *service);
+  NanoRpc::RpcServer *Build();
+};
 
 
 int wmain(int argc, wchar_t *argv[]) { 
   NanoRpc::NamedPipeRpcChannel ch("test-channel", ".");
   ch.set_connect_timeout(-1);
+  ch.set_reconnect(true);
 
-  NanoRpc::RpcServer server(ch);
-  server.RegisterService(new MyService());
-  server.Run();   // Run until connection is closed
+  ServerBuilder server_builder;
+  server_builder.RegisterService(&ch);
+  server_builder.RegisterService(new MyService());
 
-  // server.WaitForRequest
+  std::shared_ptr<NanoRpc::RpcServer> server(server_builder.Build());
+  server->Run();   // Run until connection is closed
 
+  // server->WaitForRequest();
+  // server->Shutdown();
 
 
   return 0;
