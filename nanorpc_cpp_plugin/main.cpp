27ce5b06bc9555e83d6be9e4ba460c442fe2286c
Alexandre Kozlov <alex.kozlov@oce.com>
2016-06-06 09:11:53 -0700

    added cpp generator plugin.

---
+++ nanorpc_cpp_plugin/main.cpp
@@ -0,0 +1,10 @@
+#include "google\protobuf\compiler\cpp\cpp_generator.h"
+
+namespace pb = google::protobuf::compiler::cpp;
+
+int main(int argc, char **argv) {
+  pb::CppGenerator gen;
+
+  //gen.GenerateAll()
+}
+
