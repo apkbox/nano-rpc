8646134b6248e64007c3cffd761de661abd1139b
akozl <alex.kozlov@oce.com>
2016-06-06 12:17:41 -0700

    More plugin work.

--- mk.cmd
+++ mk.cmd
@@ -1,2 +1,6 @@
-third_party\protobuf\bin\protoc rpc_proto\rpc_types.proto --cpp_out=NanoRpc\src
-rem third_party\protobuf\bin\protoc app_test.proto --cpp_out=NanoRpc_TestServer
+rem third_party\protobuf\bin\protoc rpc_proto\rpc_types.proto --cpp_out=NanoRpc\src
+third_party\protobuf\bin\protoc app_test.proto ^
+--cpp_out=NanoRpc_TestServer ^
+--plugin=protoc-gen-nanorpc_cpp=x64\Debug\nanorpc_cpp_plugin.exe --nanorpc_cpp_out=NanoRpc_TestServer
+
+
