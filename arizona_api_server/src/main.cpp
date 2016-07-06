cb33c83bc7efae276003cb5042610f347f75af1f
Alexandre Kozlov <alex.kozlov@oce.com>
2016-07-06 00:49:59 -0700

    Added Arizona test app.

---
+++ arizona_api_server/src/main.cpp
@@ -0,0 +1,17 @@
+#define _WINSOCKAPI_
+#include <process.h>
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <chrono>
+#include <thread>
+
+#include "nanorpc/nanorpc2.h"
+#include "nanorpc/client.h"
+#include "nanorpc/server.h"
+#include "nanorpc/winsock_channel.h"
+
+int main() {
+  return 0;
+}
